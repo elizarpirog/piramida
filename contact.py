ca33b51264032c9a9b112ce33d1dd6977bd40fc0
elizarpirog <elizarpir@ukr.net>
2026-07-14 19:48:38 +0300

    Add Contact class with initialization

---
+++ contact.py
@@ -0,0 +1,12 @@
+class Contact:
+
+    def __init__(
+        self,
+        name,
+        phone,
+        city
+    ):
+
+        self.name = name
+        self.phone = phone
+        self.city = city
