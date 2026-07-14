59f15abc0a404ad8edf9bd4cda93895ab8494200
elizarpirog <elizarpir@ukr.net>
2026-07-14 19:51:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
-""" Address book """
+from addressbook import AddressBook
 
-print("Project: contact-book")
+from sample_contacts import CONTACTS
+
+from exporter import show
+
+from search import by_city
+
+book = AddressBook()
+
+for contact in CONTACTS:
+
+    book.add(contact)
+
+show(book.list())
+
+print()
+
+print("People from London")
+
+print("-" * 20)
+
+for person in by_city(
+
+        book.list(),
+
+        "London"
+
+):
+
+    print(person.name)
